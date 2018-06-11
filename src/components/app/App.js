ec69ab8305a6486228a98ae409cecbaf00aa1a53
Steele Walston <steele.walston@gmail.com>
2018-06-11 16:43:56 -0700

    initial commit and base files added

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Tasha and Steele game</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
