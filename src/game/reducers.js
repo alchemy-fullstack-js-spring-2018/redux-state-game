5d9bf62ee05d741a602043425c153da5152a9348
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-11 18:08:32 -0700

    guesses reducer adds a guess

--- src/game/reducers.js
+++ src/game/reducers.js
@@ -1,5 +1,9 @@
+export const GUESS = 'GUESS';
+
 export function guesses(state = [], { type, payload }) {
   switch(type) {
+    case GUESS:
+      return [...state, payload.guess];
     default:
       return state;
   }
