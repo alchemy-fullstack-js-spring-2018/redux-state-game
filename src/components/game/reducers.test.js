331040c170b0c6e345a57deab90a67116632552c
jeffrey lonergan <jrlone1990@gmail.com>
2018-06-12 11:52:53 -0700

    three reducer tests passing

--- src/components/game/reducers.test.js
+++ src/components/game/reducers.test.js
@@ -1,12 +1,39 @@
-import { getGameState, newMatch, handleGame, NEW_GAME, NEW_ROUND, NEW_GUESS, TALLY_ROUND } from './reducers';
+import { getGameState, newMatch, handleGame, NEW_GAME, NEW_ROUND, NEW_GUESS, TALLY_ROUND, createWordArray, GAME_STATE } from './reducers';
+
+const firstGameState = {
+    limbCount: 2,
+    word: 'solatious',
+    chosen: 'solatious',
+}
+
+const secondGameState = {
+    limbCount: 6,
+    word: 'sassy',
+    chosen: 'sassy',
+}
 
 it('has a default value of an object with properties', () => {
     const state = newMatch(undefined, {});
     expect(state).toEqual({
-        limbCount: 0,
+       limbCount: 0,
         word: '',
         chosen: '',
         guess: '',    });
   });
 
-  it('gets current s, )
+//   it('creates a word array out of avaiable words', () => {
+//       word = ['solatious', 'redneck']
+//       const state = createWordArray(getWord(state), [])
+//       expect(state).toEqual(['solatious', 'redneck']);
+
+//   });
+
+  it('make sure returning a valid Game State', () => {
+    const state = getGameState(firstGameState, {});
+    expect(state).toEqual(GAME_STATE.WIN);
+  });
+
+  it('make sure return lost game state when limbs are six', () => {
+    const state = getGameState(secondGameState, {});
+    expect(state).toEqual(GAME_STATE.LOSE);
+  });
