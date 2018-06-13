c19ba1d1f5bec11b6e94a4659e4c59961bc211ea
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-13 12:05:29 -0700

    add package for mocking local storage

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   setupTestFrameworkScriptFile: '<rootDir>/enzyme.setup.js',
   transform: {
     '.*': '<rootDir>/node_modules/jest-css-modules'
-  }
+  },
+  setupFiles: ['jest-localstorage-mock']
 };
