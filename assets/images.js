30a01e02696d2105e232f6e3860fe14be0da6bdc
Jen Lipton <jkl.lipton@gmail.com>
2018-06-13 10:05:18 -0700

    updated images

--- assets/images.js
+++
@@ -1,8 +0,0 @@
-import head from './head.png';
-import leftArm from './leftArm.png';
-import leftLeg from './leftLeg.png';
-import rightArm from './rightArm.png';
-import rightLeg from './rightLeg.png';
-import torso from './torso.png';
-
-export const limbImages = [head, torso, leftArm, rightArm, leftLeg, rightLeg];
--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Hangman from './Hangman';
 import Letters from './Letters';
 import GameStatus from './GameStatus';
+import styles from './Game.css';
 
 export default class Game extends Component {
 
   render() {
 
     return (
-      <section>
+      <section className={styles.game}>
+        <h1>Skyrim Hangman</h1>
         <Hangman />
         <Letters />
         <GameStatus />
